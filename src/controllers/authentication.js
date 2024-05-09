b792b18e6e0d9f34ccf88d054a3357d768cfb203
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-09 18:28:07 +0300

    Create authentication controller for user registration

---
+++ src/controllers/authentication.js
@@ -0,0 +1,53 @@
+const { createUser, getUserByEmail } = require("../db/users");
+const { authentication, random } = require("../helpers");
+
+export const register = async (req, res) => {
+  try {
+    const { email, password, username } = req.body;
+
+    // Check if required fields are present
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
+
+    // Check if user already exists
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+
+    // Create user
+    const salt = random();
+    await createUser({
+      email,
+      username,
+      authentication: { salt, password: authentication(salt, password) },
+    });
+
+    // Login user after registration:
+    // - Get user after creation to get the _id
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    // - Create session token
+    const sessionSalt = random();
+    user.authentication.sessionToken = authentication(
+      sessionSalt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    // - Send session token as cookie
+    res.cookie("AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
---
+++ src/helpers/index.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+const crypto = require("crypto");
+
+dotenv.config();
+
+const random = () => crypto.randomBytes(128).toString("base64");
+const authentication = (salt, password) =>
+  crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(process.env.SECRET)
+    .digest("hex");
+
+module.exports = { random, authentication };
