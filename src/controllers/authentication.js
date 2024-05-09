101b511d28b73acd9a537e1c62773f378ad165a9
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-09 18:39:42 +0300

    Add user login controller and route

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -1,6 +1,52 @@
 const { createUser, getUserByEmail } = require("../db/users");
 const { authentication, random } = require("../helpers");
 
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if required fields are present
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    // Check if user exists
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    // Check if password is correct
+    const expectedHash = authentication(user.authentication.salt, password);
+    if (user.authentication.password !== expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    // Create session token
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    // Send session token as cookie
+    res.cookie("AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
 const register = async (req, res) => {
   try {
     const { email, password, username } = req.body;
@@ -52,4 +98,4 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+module.exports = { login, register };
--- src/router/authentication.js
+++ src/router/authentication.js
@@ -1,5 +1,6 @@
-const { register } = require("../controllers/authentication");
+const { login, register } = require("../controllers/authentication");
 
 module.exports = (router) => {
   router.post("/auth/register", register); // User registration route
+  router.post("/auth/login", login); // User login route
 };
