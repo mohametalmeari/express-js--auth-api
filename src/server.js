71d407607417817dfbbbb6140716b27b5edca5ac
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-09 18:11:19 +0300

    Setup server then run: npm start

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({ credentials: true }));
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+server.listen(8080, () => {
+  console.log("Server is running on http://localhost:8080/");
+});
