a91467488e4bdb0f2c865e9ee40b64b6b21d0d2e
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-09 18:14:40 +0300

    Connect to MongoDB

--- src/server.js
+++ src/server.js
@@ -4,8 +4,12 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
+
+dotenv.config();
 
 app.use(cors({ credentials: true }));
 app.use(compression());
@@ -16,3 +20,7 @@ const server = http.createServer(app);
 server.listen(8080, () => {
   console.log("Server is running on http://localhost:8080/");
 });
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on("error", (error) => console.log(error));
